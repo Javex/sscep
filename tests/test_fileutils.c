300ff68bdb9a012262af6a2b1851c4c0f6268219
javex <florian.ruechel@gmail.com>
2013-04-05 00:14:38 +0200

    Add more test skeleton for fileutils

--- tests/test_fileutils.c
+++ tests/test_fileutils.c
@@ -5,8 +5,38 @@
 
 START_TEST(test_write_crl)
 {
-	struct scep s;
-	/* NYI */
+	// Todo: Normal run that successfully ends
+}
+END_TEST
+
+START_TEST(test_write_crl_multiple)
+{
+	// Todo: Make a CRL reply with more than one CRL. This should lead to exit
+	// code SCEP_PKISTATUS_FILE
+}
+END_TEST
+
+
+START_TEST(test_write_crl_invalid_file_write)
+{
+	// Todo: Let the output file be invalid somehow. This should lead to exit
+	// code SCEP_PKISTATUS_FILE
+}
+END_TEST
+
+START_TEST(test_compare_subject)
+{
+	// Todo: Make a normal run. Perhaps we can ignore the workaround in the
+	// function (see comments there). Or maybe we should raise the version
+	// requirement to 0.9.7f?
+
+	// Also do a failing comparison here as well.
+}
+END_TEST
+
+START_TEST(test_write_local_cert)
+{
+	// Todo: Implement a function that executes normally through.
 }
 END_TEST
 
@@ -18,6 +48,10 @@ Suite * scep_conf_suite(void)
 	TCase *tc_core = tcase_create("Core");
 
 	tcase_add_test(tc_core, test_write_crl);
+	tcase_add_test(tc_core, test_write_crl_multiple);
+	tcase_add_test(tc_core, test_write_crl_invalid_file_write);
+	tcase_add_test(tc_core, test_compare_subject);
+	tcase_add_test(tc_core, test_write_local_cert);
 
 	suite_add_tcase(s, tc_core);
 
